c8319ba9f4fd996397d2c66126321406b15bc34d
Алексей Лимонов <aalimonov_2@miem.hse.ru>
2021-03-19 22:26:20 +0300

    19.03 first lessons

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/homepage")
-def homepage():
-    return "<h1> This is my homepage <h1>"
-
-@app.route("/<name>")
-def user(name):
-    return f'Hello, fellow {name}'
+@app.route("/<info>")
+def homepage(info):
+    return render_template('index.html', content = info, r = 2)
 
 @app.route('/admin')
 def admin():
-    return redirect(url_for("homepage"))
+    return redirect(url_for("user", name="Admin!"))
 
 if __name__ == "__main__":
     app.run()
