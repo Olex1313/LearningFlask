5c725debdcf881d38b81d26428326eb6867fd7da
Алексей Лимонов <aalimonov_2@miem.hse.ru>
2021-03-21 19:15:49 +0300

    changed structure

---
+++ app/views.py
@@ -0,0 +1,23 @@
+from app import app
+from flask import render_template, redirect, url_for
+
+@app.route('/')
+@app.route('/index')
+
+def index():
+    user = {'username': 'miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }, 
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', user = user, title = 'Home', posts = posts)
