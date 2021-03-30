7b6318c2267b5d108810e1ec7a2205b6d375a57e
Алексей Лимонов <aalimonov_2@miem.hse.ru>
2021-03-30 12:33:57 +0300

    Added forms for signing in

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Here I'm gonna write some simple stuff w Flask, to get familiar with it
 
 19.03.2021 - I've learned how to use templates, redirect, write python code inside html
 21.03.2021 - I've learned how to use templates inheritance
+30.03.2021 - I've learned how to manipulate forms with Flask
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, flash, url_for
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -24,3 +25,12 @@ def index():
 @app.route('/page')
 def page():
     return render_template('page.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
